123fc5e0d2b5811500c4a583bb457920b3765311
Thibault-et-Emilie <thibault_peltier@hotmail.fr>
2024-06-10 15:41:37 +0200

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('main-nav').classList.toggle('open'); // Ajoute ou retire la classe "open" pour afficher ou masquer le menu
 });
+// Sélectionne tous les éléments de lien dans le menu de navigation
+const menuLinks = document.querySelectorAll('#main-nav ul li a');
+
+// Boucle à travers chaque lien du menu
+menuLinks.forEach(function(link) {
+    // Ajoute un événement de clic à chaque lien
+    link.addEventListener('click', function() {
+        // Retire la classe 'open' du menu de navigation lorsque tu cliques sur un lien
+        document.getElementById('main-nav').classList.remove('open');
+    });
+});
+
 
 document.getElementById('guests').addEventListener('input', function() {
     const guestCount = parseInt(this.value);
