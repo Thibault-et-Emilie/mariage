3b31d4587fe89faf9dd5e8f2777c6d5b4d2e270a
Thibault-et-Emilie <thibault_peltier@hotmail.fr>
2024-06-12 16:34:09 +0200

    Update script-form.js

--- script-form.js
+++ script-form.js
@@ -5,28 +5,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const participateNo = document.getElementById('participate-no');
     const stayOptions = document.getElementById('stay-options');
 
-    guestsInput.addEventListener('input', function() {
-        // Vider le conteneur des prénoms des invités
-        guestNamesContainer.innerHTML = '';
-        const numGuests = parseInt(guestsInput.value);
+        guestsInput.addEventListener('input', function() {
+            // Vider le conteneur des prénoms des invités
+            guestNamesContainer.innerHTML = '';
+            const numGuests = parseInt(guestsInput.value);
 
-        // Ajouter des champs de prénom en fonction du nombre d'invités
-        for (let i = 1; i < numGuests; i++) {
-            const label = document.createElement('label');
-            label.setAttribute('for', `guest-name-${i+1}`);
-            label.textContent = `Prénom et nom de l'invité ${i+1}:`;
+            // Ajouter des champs de prénom en fonction du nombre d'invités
+            for (let i = 1; i < numGuests; i++) {
+                const label = document.createElement('label');
+                label.setAttribute('for', `guest-name-${i+1}`);
+                label.textContent = `Prénom et nom de l'invité ${i+1}:`;
 
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.id = `guest-name-${i}`;
-            input.name = `guest-names[]`;
-            input.required = true;
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.id = `guest-name-${i}`;
+                input.name = `guest-names[]`;
+                input.required = true;
 
-            guestNamesContainer.appendChild(label);
-            guestNamesContainer.appendChild(input);
-        }
-    });
-
+                guestNamesContainer.appendChild(label);
+                guestNamesContainer.appendChild(input);
+            }
+        });
+    
     document.getElementById('rsvp-form').addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         document.getElementById('response').innerText = 'Merci pour votre réponse!';
     });
-});
+
 
  // Gérer l'affichage des options de durée de séjour
     participateYes.addEventListener('change', function () {
