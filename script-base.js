280399cb00552896929584d1c3b4cf182230a339
Thibault-et-Emilie <thibault_peltier@hotmail.fr>
2024-06-12 20:59:07 +0200

    Update script-base.js

--- script-base.js
+++ script-base.js
@@ -2,28 +2,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const guestTable = document.getElementById('guest-data');
 
     // Fonction pour ajouter une ligne au tableau
-    function addGuestToTable(name, email, bed) {
+    function addGuestToTable(name, email, numGuests) {
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>${name}</td>
             <td>${email}</td>
-           
+            <td>${numGuests}</td>
             <!-- Ajoutez d'autres cellules de données au besoin -->
         `;
         guestTable.appendChild(row);
     }
 
-    // Écouteur d'événements pour la soumission du formulairee
+    // Écouteur d'événements pour la soumission du formulaire
     document.getElementById('rsvp-form').addEventListener('submit', function (event) {
         event.preventDefault();
 
         // Récupérer les valeurs des champs du formulaire
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
-       
+        const numGuests = document.getElementById('guests').value;
 
         // Ajouter les données au tableau
-        addGuestToTable(name, email, bed);
+        addGuestToTable(name, email, numGuests);
 
         // Réinitialiser le formulaire si nécessaire
         this.reset();
