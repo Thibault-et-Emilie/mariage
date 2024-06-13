1c9bb8dac529ee13abaa9c8bf948eef6172bd9bb
Thibault-et-Emilie <thibault_peltier@hotmail.fr>
2024-06-13 14:20:46 +0200

    Update script-base.js

--- script-base.js
+++ script-base.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     let formData = [] ;
 
     // Fonction pour ajouter une ligne au tableau
-    function addGuestToTable(name, email, participate, dureesejour, couchage) {
+    function addGuestToTable(guestname, email, participate, dureesejour, couchage) {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${name}</td>
+            <td>${guestname}</td>
             <td>${email}</td>
             <td>${participate}</td>
             <td>${dureesejour}</td>
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
     formData = JSON.parse(localStorage.getItem('formData'));
     if (formData) {
         for (let i=0 ; 10 ; i++)  {
-        addGuestToTable(formData[i].name, formData[i].email, formData[i].participate, formData[i].dureesejour, formData[i].couchage);
+        addGuestToTable(formData[i].guestname, formData[i].email, formData[i].participate, formData[i].dureesejour, formData[i].couchage);
         }
        
 
